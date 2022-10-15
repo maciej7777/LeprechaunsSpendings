661e141edce20bdcd973b4cd401509cfcde2baee
Maciej Uniejewski <maciej.uniejewski@gmail.com>
2022-10-15 18:41:38 +0200

    Add backend skeleton

---
+++ backend/leprechaunsspendings/src/main/java/com/hacktoberfest/leprechaunsspendings/LeprechaunsspendingsApplication.java
@@ -0,0 +1,13 @@
+package com.hacktoberfest.leprechaunsspendings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeprechaunsspendingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeprechaunsspendingsApplication.class, args);
+	}
+
+}
