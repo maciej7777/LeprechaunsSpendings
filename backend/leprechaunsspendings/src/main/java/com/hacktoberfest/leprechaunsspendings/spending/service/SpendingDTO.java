9a8f880273502e583c049888eb05796506970f54
Maciej Uniejewski <maciej.uniejewski@gmail.com>
2022-10-22 20:00:42 +0200

    FEATURE-4 Return object in post response

--- backend/leprechaunsspendings/src/main/java/com/hacktoberfest/leprechaunsspendings/spending/service/SpendingDTO.java
+++ backend/leprechaunsspendings/src/main/java/com/hacktoberfest/leprechaunsspendings/spending/service/SpendingDTO.java
@@ -1,14 +1,18 @@
 package com.hacktoberfest.leprechaunsspendings.spending.service;
 
+import com.hacktoberfest.leprechaunsspendings.spending.model.Spending;
 import com.hacktoberfest.leprechaunsspendings.spending.model.SpendingType;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PastOrPresent;
+import java.math.BigDecimal;
 import java.util.Date;
+import java.util.UUID;
 
 public class SpendingDTO {
+    private UUID id;
     @NotBlank(message = "Author must be provided")
     private String author;
     @NotNull(message = "Amount and currency must be provided")
@@ -21,6 +25,26 @@ public class SpendingDTO {
     @NotNull(message = "Date must be provided")
     @PastOrPresent(message = "Date must not be from the future")
     private Date date;
+
+    private SpendingDTO(final UUID id,
+                        final String author,
+                        final MoneyDTO money,
+                        final SpendingType spendingType,
+                        final String title,
+                        final String description,
+                        final Date date) {
+        this.id = id;
+        this.author = author;
+        this.money = money;
+        this.spendingType = spendingType;
+        this.title = title;
+        this.description = description;
+        this.date = date;
+    }
+
+    public UUID getId() {
+        return id;
+    }
 
     public String getAuthor() {
         return author;
@@ -68,5 +92,64 @@ public class SpendingDTO {
 
     public void setDate(Date date) {
         this.date = date;
+    }
+
+    public static class Builder {
+        private UUID id;
+        private String author;
+        private BigDecimal amount;
+        private String currency;
+        private SpendingType spendingType;
+        private String title;
+        private String description;
+        private Date date;
+
+        public static SpendingDTO.Builder create() {
+            return new SpendingDTO.Builder();
+        }
+
+        public SpendingDTO.Builder withId(UUID id) {
+            this.id = id;
+            return this;
+        }
+
+        public SpendingDTO.Builder withAuthor(String author) {
+            this.author = author;
+            return this;
+        }
+
+        public SpendingDTO.Builder withAmount(BigDecimal amount) {
+            this.amount = amount;
+            return this;
+        }
+
+        public SpendingDTO.Builder withCurrency(String currency) {
+            this.currency = currency;
+            return this;
+        }
+
+        public SpendingDTO.Builder withSpendingType(SpendingType spendingType) {
+            this.spendingType = spendingType;
+            return this;
+        }
+
+        public SpendingDTO.Builder withTitle(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public SpendingDTO.Builder withDescription(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public SpendingDTO.Builder withDate(Date date) {
+            this.date = date;
+            return this;
+        }
+
+        public SpendingDTO build() {
+            return new SpendingDTO(id, author, new MoneyDTO(amount, currency), spendingType, title, description, date);
+        }
     }
 }
--- backend/leprechaunsspendings/src/main/java/com/hacktoberfest/leprechaunsspendings/spending/service/SpendingService.java
+++ backend/leprechaunsspendings/src/main/java/com/hacktoberfest/leprechaunsspendings/spending/service/SpendingService.java
@@ -1,17 +1,19 @@
 package com.hacktoberfest.leprechaunsspendings.spending.service;
 
+import com.hacktoberfest.leprechaunsspendings.spending.model.Spending;
 import com.hacktoberfest.leprechaunsspendings.spending.model.repo.SpendingRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SpendingService {
-    private SpendingRepository spendingRepository;
+    private final SpendingRepository spendingRepository;
 
     public SpendingService(SpendingRepository spendingRepository) {
         this.spendingRepository = spendingRepository;
     }
 
-    public void createSpending(SpendingDTO spendingDTO) {
-        spendingRepository.save(SpendingMapper.mapSpending(spendingDTO));
+    public SpendingDTO createSpending(SpendingDTO spendingDTO) {
+        Spending savedSpending = spendingRepository.save(SpendingMapper.mapSpending(spendingDTO));
+        return SpendingMapper.mapSpendingDTO(savedSpending);
     }
 }
